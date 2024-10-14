c97f2dfa8c1ea4fec2e64852b74022e67f5e226e
Thayná de Castro Medeiros <182220014@senacinformatica.br>
2024-10-14 19:12:47 -0300

    primeiro commit

---
+++ carrinho.js
@@ -0,0 +1,26 @@
+// lógica do carrinho de compras
+export class Carrinho {
+    constructor() {
+        this.produtos = []; // inicia array
+    }
+
+    
+    adicionarProduto(produtoNome) {
+        
+        if (this.produtos.includes(produtoNome)) {
+            return false; 
+        }
+        this.produtos.push(produtoNome); 
+        return true; 
+    }
+
+
+    removerProduto(produtoNome) {
+        this.produtos = this.produtos.filter(produto => produto !== produtoNome); 
+    }
+
+
+    listarProdutos() {
+        return this.produtos; 
+    }
+}
